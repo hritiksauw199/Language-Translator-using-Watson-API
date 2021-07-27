d95aa12904ea48241224c600ab765e6ad9b71469
hritiksauw199 <hrithiksauw199@gmail.com>
2021-07-28 00:25:19 +0530

    adding new file

---
+++ English to Indian Language Translator/translator2.py
@@ -0,0 +1,87 @@
+"""
+Translates user input from english language to other indian languages
+"""
+#importing all necessary libraries
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+#using watson language translate instance. You can use your own, read the readme file for further info
+URL_LT = 'https://api.kr-seo.language-translator.watson.cloud.ibm.com/instances/82d9fe65-8b30-4663-8cce-70dbbe88f547'
+APIKEY_LT = '5QUnMcdZqq_VEUEJfmIxF_VU4Hk1Ki-cShbnN4biRdIq'
+VERSION_LT = '2018-05-01'
+
+#building an autheticator and translation model
+authenticator = IAMAuthenticator(APIKEY_LT)
+language_translator = LanguageTranslatorV3(version=VERSION_LT, authenticator=authenticator)
+language_translator.set_service_url(URL_LT)
+language_translator
+
+lang1 = ["english", "English", "ENGLISH"]
+lang2 = ["hindi", "Hindi", "HINDI"]
+lang3 = ["gujarati", "Gujarati", "GUJARATI"]
+lang4 = ["malayalam", "Malayalam", "MALAYALAM"]
+lang5 = ["tamil", "Tamil", "TAMIL"]
+lang6 = ["telugu", "Telugu", "TELUGU"]
+lang7 = ["urdu", "Urdu", "URDU"]
+
+def translator_new():
+	user_input = input("Enter to which language you want to convert: ")
+	your_text = input("enter: ")
+	if user_input in lang2:
+		englishtohindi(your_text)
+	elif user_input in lang3:
+		englishtogujarati(your_text)
+	elif user_input in lang4:
+		englishtomalayalam(your_text)
+	elif user_input in lang5:
+		englishtotamil(your_text)
+	elif user_input in lang6:
+		englishtotelugu(your_text)
+	elif user_input in lang7:
+		englishtourdu(your_text)
+	else:
+		print("Enter a correct language")
+
+def englishtohindi(your_text):
+	"""Function takes user text in English and convert it in Hindi using Watson Language Translator"""
+	translation_response = language_translator.translate(text=your_text, model_id='en-hi')
+	translation = translation_response.get_result()
+	eth_translation = translation['translations'][0]['translation']
+	print(eth_translation)
+
+def englishtogujarati(your_text):
+	"""Function takes user text in English and convert it in german using Watson Language Translator"""
+	translation_response = language_translator.translate(text=your_text, model_id='en-gu')
+	translation = translation_response.get_result()
+	etg_translation = translation['translations'][0]['translation']
+	print(etg_translation)
+
+def englishtomalayalam(your_text):
+	"""Function takes user text in English and convert it in Malayalam using Watson Language Translator"""
+	translation_response = language_translator.translate(text=your_text, model_id='en-ml')
+	translation = translation_response.get_result()
+	etm_translation = translation['translations'][0]['translation']
+	print(etm_translation)
+
+def englishtotamil(your_text):
+	"""Function takes user text in English and convert it in Tamil using Watson Language Translator"""
+	translation_response = language_translator.translate(text=your_text, model_id='en-ta')
+	translation = translation_response.get_result()
+	etta_translation = translation['translations'][0]['translation']
+	print(etta_translation)
+
+def englishtotelugu(your_text):
+	"""Function takes user text in English and convert it in Telugu using Watson Language Translator"""
+	translation_response = language_translator.translate(text=your_text, model_id='en-te')
+	translation = translation_response.get_result()
+	ette_translation = translation['translations'][0]['translation']
+	print(ette_translation)
+
+def englishtourdu(your_text):
+	"""Function takes user text in English and convert it in Urdu using Watson Language Translator"""
+	translation_response = language_translator.translate(text=your_text, model_id='en-ur')
+	translation = translation_response.get_result()
+	etu_translation = translation['translations'][0]['translation']
+	print(etu_translation)
+
+translator_new()
---
+++ Language Translator/Translator.py
@@ -0,0 +1,152 @@
+"""
+Translates user input from one language to other specified languages
+"""
+#importing all necessary libraries
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+#using watson language translate instance. You can use your own, read the readme file for further info
+URL_LT = 'https://api.kr-seo.language-translator.watson.cloud.ibm.com/instances/82d9fe65-8b30-4663-8cce-70dbbe88f547'
+APIKEY_LT = '5QUnMcdZqq_VEUEJfmIxF_VU4Hk1Ki-cShbnN4biRdIq'
+VERSION_LT = '2018-05-01'
+
+#building an autheticator and translation model
+authenticator = IAMAuthenticator(APIKEY_LT)
+language_translator = LanguageTranslatorV3(version=VERSION_LT, authenticator=authenticator)
+language_translator.set_service_url(URL_LT)
+language_translator
+
+lang1 = ["english", "English", "ENGLISH"]
+lang2 = ["german", "German", "GERMAN"]
+lang3 = ["french", "French", "FRENCH"]
+lang4 = ["spanish", "Spanish", "SPANISH"]
+
+def translator():
+	user_input1 = input("Enter from which language you want to convert: ")
+	user_input2 = input("Enter to which language you want to convert: ")
+	your_text = input("enter: ")
+	if user_input1 in lang1:
+		if user_input2 in lang2:
+			englishtogerman(your_text)
+		elif user_input2 in lang3:
+			englishtofrench(your_text)
+		elif user_input2 in lang4:
+			englishtospanish(your_text)
+		else:
+			print("Enter a correct language")
+	elif user_input1 in lang2:
+		if user_input2 in lang1:
+			germantoenglish(your_text)
+		elif user_input2 in lang3:
+			germantofrench(your_text)
+		elif user_input2 in lang4:
+			print('Sorry, german to spanish not supported')
+		else:
+			print("Enter a correct language")
+	elif user_input1 in lang3:
+		if user_input2 in lang1:
+			frenchtoenglish(your_text)
+		elif user_input2 in lang2:
+			frenchtogerman(your_text)
+		elif user_input2 in lang4:
+			frenchtospanish(your_text)
+		else:
+			print("Enter a correct language")
+	elif user_input1 in lang4:
+		if user_input2 in lang1:
+			spanishtoenglish(your_text)
+		elif user_input2 in lang2:
+			print('Sorry, spanish to german not supported')
+		elif user_input2 in lang3:
+			spanishtofrench(your_text)
+		else:
+			print("Enter a correct language")
+	else:
+		print("Enter a correct language")
+
+
+def englishtogerman(your_text):
+	"""Function takes user text in English and convert it in german using Watson Language Translator"""
+	translation_response = language_translator.translate(text=your_text, model_id='en-de')
+	translation = translation_response.get_result()
+	etg_translation = translation['translations'][0]['translation']
+	print(etg_translation)
+
+def germantoenglish(your_text):
+	"""Function takes user text in German and convert it in English using Watson Language Translator"""
+	translation_response = language_translator.translate(text=your_text, model_id='de-en')
+	translation = translation_response.get_result()
+	gte_translation = translation['translations'][0]['translation']
+	print(gte_translation)
+
+def englishtofrench(your_text):
+	"""Function takes user text in English and convert it in french using Watson Language Translator"""
+	translation_response = language_translator.translate(text=your_text, model_id='en-fr')
+	translation = translation_response.get_result()
+	etf_translation = translation['translations'][0]['translation']
+	print(etf_translation)
+
+def frenchtoenglish(your_text):
+	"""Function takes user text in French and convert it in English using Watson Language Translator"""
+	translation_response = language_translator.translate(text=your_text, model_id='fr-en')
+	translation = translation_response.get_result()
+	fte_translation = translation['translations'][0]['translation']
+	print(fte_translation)
+
+def englishtospanish(your_text):
+	"""Function takes user text in English and convert it in Spanish using Watson Language Translator"""
+	translation_response = language_translator.translate(text=your_text, model_id='en-es')
+	translation = translation_response.get_result()
+	ets_translation = translation['translations'][0]['translation']
+	print(ets_translation)
+
+def spanishtoenglish(your_text):
+	"""Function takes user text in Spanish and convert it in English using Watson Language Translator"""
+	translation_response = language_translator.translate(text=your_text, model_id='es-en')
+	translation = translation_response.get_result()
+	ste_translation = translation['translations'][0]['translation']
+	print(ste_translation)
+
+def germantofrench(your_text):
+	"""Function takes user text in German and convert it in French using Watson Language Translator"""
+	translation_response = language_translator.translate(text=your_text, model_id='de-fr')
+	translation = translation_response.get_result()
+	gtf_translation = translation['translations'][0]['translation']
+	print(gtf_translation)
+
+def frenchtogerman(your_text):
+	"""Function takes user text in French and convert it in German using Watson Language Translator"""
+	translation_response = language_translator.translate(text=your_text, model_id='fr-de')
+	translation = translation_response.get_result()
+	ftg_translation = translation['translations'][0]['translation']
+	print(ftg_translation)
+
+def germantospanish(your_text):
+	"""Function takes user text in German and convert it in Spanish using Watson Language Translator"""
+	translation_response = language_translator.translate(text=your_text, model_id='de-es')
+	translation = translation_response.get_result()
+	gts_translation = translation['translations'][0]['translation']
+	print(gts_translation)
+
+def frenchtospanish(your_text):
+	"""Function takes user text in French and convert it in Spanish using Watson Language Translator"""
+	translation_response = language_translator.translate(text=your_text, model_id='fr-es')
+	translation = translation_response.get_result()
+	fts_translation = translation['translations'][0]['translation']
+	print(fts_translation)
+
+def spanishtofrench(your_text):
+	"""Function takes user text in Spanish and convert it in French using Watson Language Translator"""
+	translation_response = language_translator.translate(text=your_text, model_id='es-fr')
+	translation = translation_response.get_result()
+	stf_translation = translation['translations'][0]['translation']
+	print(stf_translation)
+
+def spanishtogerman(your_text):
+	"""Function takes user text in Spanish and convert it in German using Watson Language Translator"""
+	translation_response = language_translator.translate(text=your_text, model_id='es-de')
+	translation = translation_response.get_result()
+	stg_translation = translation['translations'][0]['translation']
+	print(stg_translation)
+
+translator()
